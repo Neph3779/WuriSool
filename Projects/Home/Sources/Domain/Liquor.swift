0db63f746d6ae18b8818e524a2c93d894715036c
Neph <tony3322@naver.com>
2023-04-05 22:24:32 +0900

    [Feat] Home Domain Liquor 제작

---
+++ Projects/Home/Sources/Domain/Liquor.swift
@@ -0,0 +1,25 @@
+//
+//  Liquor.swift
+//  HomeDomain
+//
+//  Created by 천수현 on 2023/04/04.
+//  Copyright © 2023 com.neph. All rights reserved.
+//
+
+import Foundation
+
+public struct Liquor {
+    let id: Int
+    let alcoholPercentage: String
+    let dosage: String
+    let name: String
+    let imagePath: String
+
+    init(data: [String: Any]) {
+        id = data["id"] as? Int ?? -1
+        alcoholPercentage = data["alcoholPercentage"] as? String ?? ""
+        dosage = data["dosage"] as? String ?? ""
+        name = data["name"] as? String ?? ""
+        imagePath = data["imagePath"] as? String ?? ""
+    }
+}
