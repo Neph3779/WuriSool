dfbd1d7d537f81e5b7e5e8c3c6c12c6eba8a2df8
Neph <tony3322@naver.com>
2023-04-06 16:54:40 +0900

    [Feat] CardNewsCell 제작

--- Projects/Home/Sources/Presentation/CardNewsCell.swift
+++ Projects/Home/Sources/Presentation/CardNewsCell.swift
@@ -7,7 +7,35 @@
 //
 
 import UIKit
+import SnapKit
+import Kingfisher
 
 final class CardNewsCell: UICollectionViewCell {
+
     static let reuseIdentifier = String(describing: CardNewsCell.self)
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setUpImage(imagePath: String) {
+        imageView.kf.setImage(with: URL(string: imagePath))
+    }
+
+    private func layout() {
+        contentView.addSubview(imageView)
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 }
