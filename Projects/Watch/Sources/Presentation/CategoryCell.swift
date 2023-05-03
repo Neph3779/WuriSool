848ae10455c358f9b14d903d65b9fe9a0a6e694d
Neph <tony3322@naver.com>
2023-05-03 23:50:43 +0900

    [Feat] #8 카테고리, 비디오 셀 UI 제작

--- Projects/Watch/Sources/Presentation/CategoryCell.swift
+++ Projects/Watch/Sources/Presentation/CategoryCell.swift
@@ -12,11 +12,56 @@ final class CategoryCell: UICollectionViewCell {
 
     static let reuseIdentifier = String(describing: CategoryCell.self)
 
+    override var isSelected: Bool {
+        didSet {
+            isSelected ? setUpSelected() : setUpDeselected()
+        }
+    }
+
+    private let categoryLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = DesignAsset.Colors.gray4.color
+        label.applyFont(font: .buttonMedium)
+        return label
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
+        layout()
+        setUpDeselected()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    func setUpContents(category: LiquorType) {
+        categoryLabel.text = category.name
+    }
+
+    private func layout() {
+        contentView.addSubview(categoryLabel)
+        categoryLabel.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(12)
+            $0.top.bottom.equalToSuperview().inset(8)
+        }
+    }
+
+    private func setUpDeselected() {
+        contentView.backgroundColor = .white
+        layer.cornerRadius = 8
+        layer.borderColor = DesignAsset.Colors.gray2.color.cgColor
+        categoryLabel.textColor = DesignAsset.Colors.gray4.color
+        layer.borderWidth = 1
+        clipsToBounds = true
+    }
+
+    private func setUpSelected() {
+        contentView.backgroundColor = DesignAsset.Colors.primary1.color
+        layer.cornerRadius = 8
+        layer.borderColor = DesignAsset.Colors.primary8.color.cgColor
+        layer.borderWidth = 1
+        categoryLabel.textColor = DesignAsset.Colors.primary10.color
+        clipsToBounds = true
     }
 }
--- Projects/Watch/Sources/Presentation/VideoCell.swift
+++ Projects/Watch/Sources/Presentation/VideoCell.swift
@@ -11,12 +11,71 @@ import UIKit
 final class VideoCell: UICollectionViewCell {
 
     static let reuseIdentifier = String(describing: VideoCell.self)
+    private let thumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 10
+        let youtubeImageView = UIImageView()
+        youtubeImageView.image = DesignAsset.Images.youtube.image
+        imageView.addSubview(youtubeImageView)
+        youtubeImageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        return imageView
+    }()
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.applyFont(font: .bodySmall)
+        return label
+    }()
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 12
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    private let channelNameLabel: UILabel = {
+        let label = UILabel()
+        label.applyFont(font: .captionLarge)
+        label.textColor = DesignAsset.Colors.gray5.color
+        return label
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        layout()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    func setUpContents() {
+
+    }
+
+    private func layout() {
+        [thumbnailImageView, titleLabel, profileImageView, channelNameLabel].forEach {
+            contentView.addSubview($0)
+        }
+        thumbnailImageView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(190)
+        }
+        titleLabel.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(thumbnailImageView.snp.bottom).offset(4)
+        }
+        profileImageView.snp.makeConstraints {
+            $0.width.height.equalTo(24)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(4)
+            $0.leading.equalToSuperview()
+        }
+        channelNameLabel.snp.makeConstraints {
+            $0.leading.equalTo(profileImageView.snp.trailing).offset(4)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(4)
+            $0.trailing.equalToSuperview()
+        }
     }
 }
