97cfa825797da159d1592624fee01bedcc96ecd9
Neph <tony3322@naver.com>
2023-05-12 23:13:38 +0900

    [Feat] #10 링크, 전화번호 이동 및 팝업 구현

--- Projects/Brewery/Sources/Presentation/Common/InfoView.swift
+++ Projects/Brewery/Sources/Presentation/Common/InfoView.swift
@@ -16,13 +16,15 @@ final class InfoView: UIView {
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
-    private let descriptionLabel: UILabel = {
+    private lazy var descriptionLabel: UILabel = {
         let label = UILabel()
         label.applyFont(font: .bodySmall)
         label.textColor = DesignAsset.Colors.gray6.color
         label.numberOfLines = 0
         label.lineBreakMode = .byTruncatingTail
         label.lineBreakStrategy = .hangulWordPriority
+        label.isUserInteractionEnabled = true
+        label.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(descriptionLabelTapped(_:))))
         return label
     }()
     lazy var copyButton: UIButton = {
@@ -42,10 +44,23 @@ final class InfoView: UIView {
         descriptionLabel.text = description
         layout()
         copyButton.isHidden = !isCopyEnable
+        setLinkMoves()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setLinkMoves() {
+        guard let description = descriptionLabel.text else { return }
+        if !description.isEmpty,
+           let url = URL(string: "tel://\(description)"),
+           UIApplication.shared.canOpenURL(url) {
+            descriptionLabel.underline = .single
+        } else if let url = URL(string: description),
+                  UIApplication.shared.canOpenURL(url) {
+            descriptionLabel.underline = .single
+        }
     }
 
     private func layout() {
@@ -67,6 +82,22 @@ final class InfoView: UIView {
             $0.centerY.equalToSuperview()
             $0.trailing.equalToSuperview().inset(15)
             $0.height.width.equalTo(16)
+        }
+    }
+
+    @objc
+    private func descriptionLabelTapped(_ sender: UIGestureRecognizer) {
+        guard let description = descriptionLabel.text else { return }
+        let phoneNumber = description.filter { $0.isWholeNumber }
+        if !phoneNumber.isEmpty,
+           let url = URL(string: "tel://\(phoneNumber)"),
+           UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            descriptionLabel.underline = .single
+        } else if let url = URL(string: description),
+                  UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url)
+            descriptionLabel.underline = .single
         }
     }
 }
